8ede0923da21b843b13f52386f02d1fcec834eb6
= <atulsingh1396@gmail.com>
2019-11-30 01:22:45 +0530

    added the bucket and todo model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Bucket(models.Model):
+    bucket_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.bucket_name
+
+
+class Todo(models.Model):
+    job_todo = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
+    bucket = models.ForeignKey(Bucket, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.job_todo
