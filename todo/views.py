0ec6a8cbb96c1d069e4568198a51cca259e78095
= <atulsingh1396@gmail.com>
2019-11-30 13:51:05 +0530

    added viewset and queryset for the serializer

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import TodoSerializer
+from .models import Bucket, Todo
 
-# Create your views here.
+# Creating a bucket:
+#   from todo.models import Bucket, Todo
+#   b1 = Bucket(bucket_name="customBucket")
+#   b1.save()
+# Creating a todo inside the bucket:
+#   b1.todos.create(job_todo="do the job")
+
+
+class TodoView(viewsets.ModelViewSet):
+    serializer_class = TodoSerializer
+    queryset = Bucket.objects.all()
